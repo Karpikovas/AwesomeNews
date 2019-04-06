67302e1ecb9b1ae0e018e61f11d2d9891829149b
karpikovasophie <karpikovasophie@gmail.com>
2019-04-06 09:31:50 +0300

    First build 2

---
+++ frontend/build/precache-manifest.c56ac7be434171a7aa54beadec69624e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "db44058d88c16e0cc328c74604025195",
+    "url": "/static/media/фон.db44058d.svg"
+  },
+  {
+    "revision": "3f748cbe93367f02d52b16f170546684",
+    "url": "/static/media/logo.3f748cbe.svg"
+  },
+  {
+    "revision": "71e7f4bd2e94f0eee89b6376eaa17562",
+    "url": "/static/media/back.71e7f4bd.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8b705f8bc9e6e024533c",
+    "url": "/static/js/main.8b705f8b.chunk.js"
+  },
+  {
+    "revision": "ff605259a3f152d77eac",
+    "url": "/static/js/2.ff605259.chunk.js"
+  },
+  {
+    "revision": "8b705f8bc9e6e024533c",
+    "url": "/static/css/main.a00c81fb.chunk.css"
+  },
+  {
+    "revision": "ef396e7231ff7286506e05d5db5816ea",
+    "url": "/index.html"
+  }
+];
